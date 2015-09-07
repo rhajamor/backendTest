a5bb17d8d731272d0c463c10658b751a3c9f5976
unknown <RH030971@CERN-RH030971.northamerica.cerner.net>
2015-09-07 17:30:56 +0200

    github import

---
+++ src/main/java/com/creativeaugen/controllers/RestController.java
@@ -0,0 +1,28 @@
+package com.creativeaugen.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.creativeaugen.models.User;
+import com.creativeaugen.services.DataService;
+
+
+@Controller
+@RequestMapping("/api/users")
+public class RestController {
+
+	@Autowired
+	DataService dataService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public List<User> list() {
+		return dataService.getUserList();
+
+	}
+}
---
+++ src/main/java/com/creativeaugen/services/DataService.java
@@ -0,0 +1,9 @@
+package com.creativeaugen.services;
+
+import java.util.List;
+
+import com.creativeaugen.models.User;
+
+public interface DataService {
+	public List<User> getUserList();
+}
